c0b83e912fe2e5be01ccdd3252b8c138e576c135
Federico Herrera <jfh1163@gmail.com>
2024-04-10 21:48:46 -0300

    Creación de componentes varios y renderizado de los mismos

---
+++ src/components/Title/Title.js
@@ -0,0 +1,9 @@
+import './Title.module.css';
+
+const Title = ({text}) => {
+  return (
+    <h1>{text}</h1>
+  );
+};
+
+export default Title;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,11 @@
+import './Home.module.css';
+
+import Title from '../../components/Title/Title'
+
+const Home = () => {
+  return (
+    <Title text="Hola Mundo!" />
+  );
+};
+
+export default Home;
